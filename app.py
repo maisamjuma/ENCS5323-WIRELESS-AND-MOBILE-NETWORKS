0da8e680552869f2c566b42d70d68c336fa2667e
Maisam Alaa <52468869+maisamjuma@users.noreply.github.com>
2024-06-13 16:08:22 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/page1')
+def page1():
+    return render_template('page1.html')
+
+@app.route('/page2')
+def page2():
+    return render_template('page2.html')
+
+@app.route('/page3')
+def page3():
+    return render_template('page3.html')
+
+@app.route('/page4')
+def page4():
+    return render_template('page4.html')
+
+@app.route('/page5')
+def page5():
+    return render_template('page5.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
